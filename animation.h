e5ce74c251557af42ed8a0d1b8bb67f28bbada9b
chrisomatic <chrisrose708@gmail.com>
2022-06-08 15:02:14 -0400

    Started animation system

---
+++ animation.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define MAX_KEYFRAMES_PER_ANIMATION 10
+
+typedef struct
+{
+    Vector3f position;
+    Quaternion rotation;
+    float duration;
+} Keyframe;
+
+typedef struct
+{
+    Keyframe keyframes[MAX_KEYFRAMES_PER_ANIMATION];
+    int curr_keyframe_index;
+    int keyframe_count;
+
+    float elapsed_time; // for current keyframe only
+
+    int loops_completed;
+    int loop_count;
+} Animation;
--- weapon.c
+++ weapon.c
@@ -2,6 +2,7 @@
 #include "3dmath.h"
 #include "model.h"
 #include "player.h"
+#include "animation.h"
 #include "util.h"
 
 #include "weapon.h"
@@ -9,15 +10,7 @@
 GLuint t_claymore;
 Model m_claymore;
 
-typedef struct
-{
-    Vector3f position;
-    Quaternion rotation;
-} WeaponKeyframe;
-
-WeaponKeyframe rest;
-WeaponKeyframe windup;
-WeaponKeyframe attack;
+static Animation melee_anim;
 
 static void move_up(Vector3f* v, float val)
 {
@@ -43,11 +36,11 @@ static void move_right(Vector3f* v, float val)
     subtract(v,right);
 }
 
-static void move(Vector3f* v, Vector3f* t)
+static void move(Vector3f* v, float x, float y, float z)
 {
-    move_up(v, t->y);
-    move_forward(v, t->x);
-    move_right(v, t->z);
+    move_up(v, y);
+    move_forward(v, x);
+    move_right(v, z);
 }
 
 void weapon_init()
@@ -57,30 +50,68 @@ void weapon_init()
     model_import(&m_claymore,"models/claymore.obj");
     m_claymore.texture = t_claymore;
 
-    // init keyframes
-    rest.position.x = 0.30; // forward
-    rest.position.y = 1.20; // up
-    rest.position.z = 0.20; // right
+    // init animation
+    melee_anim.keyframes[0].position.x = 0.30;
+    melee_anim.keyframes[0].position.y = 1.20;
+    melee_anim.keyframes[0].position.z = 0.20;
+    melee_anim.keyframes[0].duration   = 0.00;
 
-    windup.position.x = -0.10; // forward
-    windup.position.y = 0.20; // up
-    windup.position.z = 0.20; // right
+    melee_anim.keyframes[1].position.x = 0.20;
+    melee_anim.keyframes[1].position.y = 1.40;
+    melee_anim.keyframes[1].position.z = 0.40;
+    melee_anim.keyframes[1].duration   = 0.20;
 
-    attack.position.x = 0.10; // forward
-    attack.position.y = -0.10; // up
-    attack.position.z = -0.30; // right
+    melee_anim.keyframes[2].position.x = 0.40;
+    melee_anim.keyframes[2].position.y = 1.10;
+    melee_anim.keyframes[2].position.z = -0.10;
+    melee_anim.keyframes[2].duration   = 0.70;
+
+    melee_anim.elapsed_time = 0.00;
+    melee_anim.keyframe_count = 3;
+    melee_anim.curr_keyframe_index = 0;
+    melee_anim.loop_count = 0;
+    melee_anim.loops_completed = 0;
 }
 
 void weapon_update(Weapon* w)
 {
-
     Vector3f sca = {1.0,1.0,1.0};
     Vector3f rot = {0.4*player->angle_v,-player->angle_h,0.0};
     Vector3f pos = {-player->phys.pos.x,-player->phys.pos.y,-player->phys.pos.z};
 
-    move(&pos, &rest.position);
-    //move(&pos, &attack.position);
-    //move(&pos, &windup.position);
+    if(player->state == PLAYER_STATE_NORMAL)
+    {
+        Keyframe* rest = &melee_anim.keyframes[0];
+        move(&pos, rest->position.x,rest->position.y,rest->position.z);
+    }
+    else
+    {
+        int next_index = (melee_anim.curr_keyframe_index + 1 >= melee_anim.keyframe_count ? 0 : melee_anim.curr_keyframe_index + 1);
+
+        Keyframe* curr_keyframe = &melee_anim.keyframes[melee_anim.curr_keyframe_index];
+        Keyframe* next_keyframe = &melee_anim.keyframes[next_index];
+
+        float t = (melee_anim.elapsed_time / curr_keyframe->duration);
+        t = MIN(t, 1.0);
+
+        float x = ((1.0 - t)*curr_keyframe->position.x) + ((t)*next_keyframe->position.x);
+        float y = ((1.0 - t)*curr_keyframe->position.y) + ((t)*next_keyframe->position.y);
+        float z = ((1.0 - t)*curr_keyframe->position.z) + ((t)*next_keyframe->position.z);
+
+        move(&pos, x,y,z);
+
+        melee_anim.elapsed_time += g_delta_t;
+        if(melee_anim.elapsed_time >= curr_keyframe->duration)
+        {
+            melee_anim.curr_keyframe_index++;
+            if(melee_anim.curr_keyframe_index >= melee_anim.keyframe_count)
+            {
+                melee_anim.curr_keyframe_index = 0;
+            }
+            melee_anim.elapsed_time = 0.00;
+        }
+    }
+
 
     get_model_transform(&pos,&rot,&sca,&w->model.transform);
 }
